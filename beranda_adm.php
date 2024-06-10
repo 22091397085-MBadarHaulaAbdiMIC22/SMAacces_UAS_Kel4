b006a9de9094639a02e9cd9b7c387e56c14ecd7c
HaulaAbdii <muhammadbadar.22085@mhs.unesa.ac.id>
2024-06-10 23:10:35 +0700

    first commit

---
+++ beranda_adm.php
@@ -0,0 +1,32 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                <h5>Web</h5>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 align="center">Halaman Administrator</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="./img/sekolah.png" width="200px" height="200px"></p>
+                          <p class="label-title" align="center"><strong><b>Tugas Uas SIM Kelompok 4</b></strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                           <br>sebuah platform berbasis web yang dirancang khusus untuk membantu siswa-siswi Sekolah Menengah Atas (SMA) dalam mengelola berbagai informasi terkait kegiatan sekolah dan persiapan menuju perguruan tinggi. Dengan fitur-fitur yang komprehensif, 
+                           siswa dapat dengan mudah mengakses dan memanfaatkan berbagai layanan yang disediakan, mulai dari manajemen data pribadi, transkrip nilai, jadwal pembelajaran, hingga rekomendasi Perguruan Tinggi Negeri (PTN) yang sesuai dengan minat dan prestasi mereka.Selain itu, SMAcces juga menyediakan fasilitas untuk mengelola pembayaran SPP, informasi mengenai jurusan dan kegiatan sekolah, absensi siswa, serta daftar guru yang bertugas di sekolah <br>
+                          </p>
+
+                         </thead
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
